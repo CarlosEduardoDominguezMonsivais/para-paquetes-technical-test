24f962b76c4bf9e251243e939b6b974108679294
CarlosEduardoDominguezMonsivais <carlos1726257@gmail.com>
2022-06-10 00:33:42 -0500

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,74 @@
 <template>
-  <nav>
-    <router-link to="/">Inicio</router-link> |
-    <router-link to="/pays">Ordenes de compra</router-link>
-  </nav>
+  <div class="bg-indigo-600">
+    <nav
+      class="
+        container
+        px-6
+        py-8
+        mx-auto
+        md:flex md:justify-between md:items-center
+      "
+    >
+      <div class="flex items-center justify-between">
+        <router-link
+          to="/"
+          class="
+            text-xl
+            font-bold
+            text-gray-100
+            md:text-2xl
+            hover:text-indigo-400
+          "
+          >
+          <img alt="logo" src="./assets/parapaquetes.svg">
+        </router-link>
+        <!--  Menu para movil -->
+        <div @click="toggleNav" class="flex md:hidden">
+          <button
+            type="button"
+            class="
+              text-gray-100
+              hover:text-gray-400
+              focus:outline-none focus:text-gray-400
+            "
+          >
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <!-- Menu para movil open: "block", Menu closed: "hidden" -->
+      <ul
+        :class="showMenu ? 'flex' : 'hidden'"
+        class="
+          flex-col
+          mt-8
+          space-y-4
+          md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
+        "
+      >
+        <router-link to="/" class="text-gray-100 hover:text-indigo-400">Inicio</router-link> 
+        <router-link to="/pays" class="text-gray-100 hover:text-indigo-400">Ordenes de compra</router-link>
+      </ul>
+    </nav>
+  </div>
   <router-view/>
 </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
+
+};
+</script>
